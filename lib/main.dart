14b4e05d030305ee816ba08004178a6c1ac748c9
Kushagra Singh <kushthaksh@gmail.com>
2020-11-11 15:36:29 -0800

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Disce',
+      theme: ThemeData(
+        primarySwatch: Colors.pink[200],
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: App(),
+    );
+  }
+}
+
+class App extends StatefulWidget {
+  @override
+  _AppState createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
